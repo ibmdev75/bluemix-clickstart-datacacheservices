ee7c692d64113cb075d5053e3ac0bd6241da3ddb
ibmdev75 <ibmdev75@gmail.com>
2014-06-15 19:16:16 +0200

    Intégration Bootstrap et JQuery

---
+++ src/test/java/dataCacheService/DataCacheServiceTest.java
@@ -0,0 +1,15 @@
+package dataCacheService;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+
+public class DataCacheServiceTest extends TestCase {
+
+	@Test
+	public void test() {
+		//ObjectGridManager ogm = ObjectGridManagerFactory.getObjectGridManager();
+		System.out.println("test : ");
+	}
+
+}
